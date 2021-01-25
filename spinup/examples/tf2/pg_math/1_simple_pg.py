60c6766c97e9e3baaf6a2156a7532a2506e39482
JordLee <unghee@umich.edu>
2021-01-25 00:11:32 -0500

    init

---
+++ spinup/examples/tf2/pg_math/1_simple_pg.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tfp.distributions import Categorical
+
+import numpy as np
+import gym
+from gym.spaces import Discrete, Box
+
+def mlp(sizes, activation='tanh', output_activation='linear'):
+    # Build a feedforward neural network.
+    layers = []
+    layers += [tf.keras.Input(shape=(sizes[0]))]
+    for j in range(len(sizes)-1):
+        act = activation if j < len(sizes)-2 else output_activation
+        layers += [tf.keras.layers.Dense(sizes[j+1], act)]
+    return nn.Sequential(*layers)
+
+def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, 
+          epochs=50, batch_size=5000, render=False):
+
+    # make environment, check spaces, get obs / act dims
+    env = gym.make(env_name)
+    assert isinstance(env.observation_space, Box), \
+        "This example only works for envs with continuous state spaces."
+    assert isinstance(env.action_space, Discrete), \
+        "This example only works for envs with discrete action spaces."
+
+    obs_dim = env.observation_space.shape[0]
+    n_acts = env.action_space.n
+
+    # make core of policy network
+    logits_net = mlp(sizes=[obs_dim]+hidden_sizes+[n_acts])
+
+    # make function to compute action distribution
+    def get_policy(obs):
+        logits = logits_net(obs)
+        return Categorical(logits=logits)
+
+    # make action selection function (outputs int actions, sampled from policy)
+    def get_action(obs):
+        return get_policy(obs).sample().numpy()
+
+    # make loss function whose gradient, for the right data, is policy gradient
+    def compute_loss(obs, act, weights):
+        logp = get_policy(obs).log_prob(act)
+        return -(logp * weights).mean()  
