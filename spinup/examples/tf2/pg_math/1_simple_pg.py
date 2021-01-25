ce808ab656ffb5aeba5d1e05bd7a9e50b3d04381
JordLee <unghee@umich.edu>
2021-01-25 01:47:24 -0500

    changed all to tf, error need to be fixed

--- spinup/examples/tf2/pg_math/1_simple_pg.py
+++ spinup/examples/tf2/pg_math/1_simple_pg.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
-from tfp.distributions import Categorical
+from tensorflow.keras.optimizers import Adam
 
 import numpy as np
 import gym
@@ -13,7 +13,7 @@ def mlp(sizes, activation='tanh', output_activation='linear'):
     for j in range(len(sizes)-1):
         act = activation if j < len(sizes)-2 else output_activation
         layers += [tf.keras.layers.Dense(sizes[j+1], act)]
-    return nn.Sequential(*layers)
+    return tf.keras.Sequential(layers)
 
 def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, 
           epochs=50, batch_size=5000, render=False):
@@ -34,7 +34,7 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
     # make function to compute action distribution
     def get_policy(obs):
         logits = logits_net(obs)
-        return Categorical(logits=logits)
+        return tfp.distributions.Categorical(logits=logits)
 
     # make action selection function (outputs int actions, sampled from policy)
     def get_action(obs):
@@ -43,4 +43,87 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
     # make loss function whose gradient, for the right data, is policy gradient
     def compute_loss(obs, act, weights):
         logp = get_policy(obs).log_prob(act)
-        return -(logp * weights).mean()  
+        return -(logp * weights).mean() 
+
+    # make optimizer
+    optimizer = Adam(learning_rate=lr)
+
+    # for training policy
+    def train_one_epoch():
+        # make some empty lists for logging.
+        batch_obs = []          # for observations
+        batch_acts = []         # for actions
+        batch_weights = []      # for R(tau) weighting in policy gradient
+        batch_rets = []         # for measuring episode returns
+        batch_lens = []         # for measuring episode lengths
+
+        # reset episode-specific variables
+        obs = env.reset()       # first obs comes from starting distribution
+        done = False            # signal from environment that episode is over
+        ep_rews = []            # list for rewards accrued throughout ep
+
+        # render first episode of each epoch
+        finished_rendering_this_epoch = False
+
+        # collect experience by acting in the environment with current policy
+        while True:
+
+            # rendering
+            if (not finished_rendering_this_epoch) and render:
+                env.render()
+
+            # save obs
+            batch_obs.append(obs.copy())
+
+            # act in the environment
+            act = get_action(tf.convert_to_tensor(obs, dtype=tf.float32))
+            obs, rew, done, _ = env.step(act)
+
+            # save action, reward
+            batch_acts.append(act)
+            ep_rews.append(rew)
+
+            if done:
+                # if episode is over, record info about episode
+                ep_ret, ep_len = sum(ep_rews), len(ep_rews)
+                batch_rets.append(ep_ret)
+                batch_lens.append(ep_len)
+
+                # the weight for each logprob(a|s) is R(tau)
+                batch_weights += [ep_ret] * ep_len
+
+                # reset episode-specific variables
+                obs, done, ep_rews = env.reset(), False, []
+
+                # won't render again this epoch
+                finished_rendering_this_epoch = True
+
+                # end experience loop if we have enough of it
+                if len(batch_obs) > batch_size:
+                    break
+
+        # take a single policy gradient update step
+        batch_loss = compute_loss(obs=tf.convert_to_tensor(batch_obs, dtype=tf.float32),
+                                  act=tf.convert_to_tensor(batch_acts, dtype=tf.int32),
+                                  weights=tf.convert_to_tensor(batch_weights, dtype=tf.float32)
+                                  )
+        var_list_fn = lambda: logits_net.trainable_weights 
+        optimizer.minimize(batch_loss, var_list_fn)
+
+        return batch_loss, batch_rets, batch_lens
+
+    # training loop
+    for i in range(epochs):
+        batch_loss, batch_rets, batch_lens = train_one_epoch()
+        print('epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f'%
+                (i, batch_loss, np.mean(batch_rets), np.mean(batch_lens)))
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env_name', '--env', type=str, default='CartPole-v0')
+    parser.add_argument('--render', action='store_true')
+    parser.add_argument('--lr', type=float, default=1e-2)
+    args = parser.parse_args()
+    print('\nUsing simplest formulation of policy gradient.\n')
+    train(env_name=args.env_name, render=args.render, lr=args.lr)
